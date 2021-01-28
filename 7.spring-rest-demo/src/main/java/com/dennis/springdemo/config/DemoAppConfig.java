5a419b36f9ee8d9671b32e1b9bd0e8091c0f850a
Dennis Cho <cho.evergreen@gmail.com>
2021-01-28 16:14:59 +0900

    exception handling

---
+++ 7.spring-rest-demo/src/main/java/com/dennis/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.dennis.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.dennis.springdemo")
+public class DemoAppConfig {
+
+}
